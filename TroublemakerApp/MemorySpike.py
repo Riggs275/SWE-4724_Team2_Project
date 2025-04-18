7221c8d04b62d57ba08cbd8f6095f553d54532df
Evalenc6 <evanv.prof@gmail.com>
2025-04-18 14:45:24 -0400

    increased how long the system will hold the memory during the memory spike for grafana and prometheus to see it

--- TroublemakerApp/MemorySpike.py
+++ TroublemakerApp/MemorySpike.py
@@ -69,7 +69,7 @@ class MemorySpike(Event):
 
 
             spike_data = [bytearray(self.CHUNK_SIZE) for _ in range(num_chunks)]
-            time.sleep(5)
+            time.sleep(15)
 
         except MemoryError:
             print("Memory spike caused an error!")
