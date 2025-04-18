9a84b91f00342097c8605741f3bfee72ad8141a5
Evalenc6 <evanv.prof@gmail.com>
2025-04-18 07:46:42 -0400

    Fixed code

--- TroublemakerApp/checkDirectoryOverflow.py
+++ TroublemakerApp/checkDirectoryOverflow.py
@@ -1,12 +1,15 @@
 import os
 
-DIR_TO_MONITOR = "/home/EValenc6/tmp-stress"
-MAX_FILES = 100
+DIR_TO_MONITOR = "/home/EValenc6/SWE-4724_Team2_Project/TroublemakerApp/overflow_test_dir"
 OUTPUT_FILE = "/home/EValenc6/owl-monitoring/text-metrics/directory_overflow.prom"
 
-file_count = len(os.listdir(DIR_TO_MONITOR))
+try:
+    file_count = len(os.listdir(DIR_TO_MONITOR))
+except FileNotFoundError:
+    file_count = 0
 
 with open(OUTPUT_FILE, "w") as f:
-    f.write(f'# HELP directory_file_count Number of files in monitored directory\n')
-    f.write(f'# TYPE directory_file_count gauge\n')
-    f.write(f'directory_file_count {file_count}\n')
+    f.write("# HELP directory_file_count Number of files in overflow test dir\n")
+    f.write("# TYPE directory_file_count gauge\n")
+    f.write(f"directory_file_count {file_count}\n")
+#/Users/evanv/owlEye/SWE-4724_Team2_Project/TroublemakerApp/checkDirectoryOverflow.py
